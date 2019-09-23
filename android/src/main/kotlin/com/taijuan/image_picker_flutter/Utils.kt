f63b140f49a4c04a48bbf988d802f2020a62384a
zhengtaijuan <673636090@qq.com>
2019-09-23 15:06:48 +0800

    android 10

--- android/src/main/kotlin/com/taijuan/image_picker_flutter/Utils.kt
+++ android/src/main/kotlin/com/taijuan/image_picker_flutter/Utils.kt
@@ -18,13 +18,6 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 /**
- * 判断SDCard是否可用
- */
-internal fun existSDCard(): Boolean {
-    return Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
-}
-
-/**
  * 默认情况下，即不需要指定intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
  * 照相机有自己默认的存储路径，拍摄的照片将返回一个缩略图。如果想访问原始图片，
  * 可以通过dat extra能够得到原始图片位置。即，如果指定了目标uri，data就没有数据，
@@ -33,12 +26,13 @@ internal fun existSDCard(): Boolean {
  * 7.0 调用系统相机拍照不再允许使用Uri方式，应该替换为FileProvider
  */
 internal fun PluginRegistry.Registrar.takePicture(result: MethodChannel.Result) {
-    var takeImageFile = if (existSDCard()) {
-        File(context().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "/DCIM/camera/")
+
+    var takeImageFile = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
     } else {
-        Environment.getDataDirectory()
-    }
-    takeImageFile = createFile(takeImageFile, "IMG_", ".jpg")
+        context().getExternalFilesDir(Environment.DIRECTORY_DCIM)
+    } ?: context().filesDir
+    takeImageFile = createFile(takeImageFile, "IMG-", ".jpg")
     val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
     takePictureIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
     if (takePictureIntent.resolveActivity(activity().packageManager) != null) {
@@ -50,27 +44,29 @@ internal fun PluginRegistry.Registrar.takePicture(result: MethodChannel.Result) 
         }
         takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
         activity().startActivityForResult(takePictureIntent, REQUEST_CAMERA_IMAGE)
-    }
-    resultListener.onCompleted = {
-        MediaScannerConnection.scanFile(context().applicationContext, arrayOf(takeImageFile.toString()), null) { path, _ ->
-            if (path == takeImageFile.toString()) {
-                val imageItem = HashMap<String, Any>().apply {
-                    put("id", path)
-                    put("name", takeImageFile.name)
-                    put("path", path)
-                    put("mimeType", "image/jpg")
-                    put("time", System.currentTimeMillis())
-                    val arr = size(path)
-                    Log.e("zuiweng", arr.toString())
-                    put("width", arr[0])
-                    put("height", arr[1])
+
+        resultListener.onCompleted = {
+            MediaScannerConnection.scanFile(context().applicationContext, arrayOf(takeImageFile.toString()), arrayOf("image/*")) { path, _ ->
+                path.logcat()
+                if (path == takeImageFile.toString()) {
+                    val imageItem = HashMap<String, Any>().apply {
+                        put("id", path)
+                        put("name", takeImageFile.name)
+                        put("path", path)
+                        put("mimeType", "image/jpg")
+                        put("time", System.currentTimeMillis())
+                        val arr = size(path)
+                        Log.e("zuiweng", arr.toString())
+                        put("width", arr[0])
+                        put("height", arr[1])
+                    }
+
+                    this.activity().runOnUiThread {
+                        result.success(imageItem)
+                    }
                 }
 
-                this.activity().runOnUiThread {
-                    result.success(imageItem)
-                }
             }
-
         }
     }
 }
@@ -84,12 +80,12 @@ internal fun PluginRegistry.Registrar.takePicture(result: MethodChannel.Result) 
  * 7.0 调用系统相机拍照不再允许使用Uri方式，应该替换为FileProvider
  */
 internal fun PluginRegistry.Registrar.takeVideo(result: MethodChannel.Result) {
-    var takeImageFile = if (existSDCard()) {
-        File(context().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "/DCIM/camera/")
+    var takeImageFile = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES)
     } else {
-        Environment.getDataDirectory()
-    }
-    takeImageFile = createFile(takeImageFile, "VIDEO_", ".mp4")
+        context().getExternalFilesDir(Environment.DIRECTORY_MOVIES)
+    } ?: context().filesDir
+    takeImageFile = createFile(takeImageFile, "VIDEO-", ".mp4")
     val takePictureIntent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
     takePictureIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
     if (takePictureIntent.resolveActivity(activity().packageManager) != null) {
@@ -103,7 +99,7 @@ internal fun PluginRegistry.Registrar.takeVideo(result: MethodChannel.Result) {
         activity().startActivityForResult(takePictureIntent, REQUEST_CAMERA_VIDEO)
     }
     resultListener.onCompleted = {
-        MediaScannerConnection.scanFile(context().applicationContext, arrayOf(takeImageFile.toString()), null) { path, _ ->
+        MediaScannerConnection.scanFile(context().applicationContext, arrayOf(takeImageFile.toString()), arrayOf("video/*")) { path, _ ->
             if (path == takeImageFile.toString()) {
                 val imageItem = HashMap<String, Any>().apply {
                     put("id", path)
@@ -128,7 +124,7 @@ internal fun PluginRegistry.Registrar.takeVideo(result: MethodChannel.Result) {
  */
 private fun createFile(folder: File, prefix: String, suffix: String): File {
     if (!folder.exists() || !folder.isDirectory) folder.mkdirs()
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd_HH:mm:ss", Locale.CHINA)
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault())
     val filename = prefix + dateFormat.format(Date(System.currentTimeMillis())) + suffix
     return File(folder, filename)
 }
